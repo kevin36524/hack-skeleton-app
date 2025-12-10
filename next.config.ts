dd67679f68a33e4fec0cac19aa59725afe852d17
Kevin Patel <kevin36524@gmail.com>
2025-12-10 15:10:19 +0530

    Now using tubopack and also using libsql/client

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@mastra/libsql', '@libsql/client'],
+  },
+  turbopack: {},
 };
 
 export default nextConfig;
