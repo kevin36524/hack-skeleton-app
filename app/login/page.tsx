0155235d3414b5a5ed5736707461868123f0c8da
Sandbox <sandbox@e2b.dev>
2026-02-06 20:13:13 +0000

    just test changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -103,7 +103,7 @@ function LoginForm() {
             <Mail className="h-8 w-8 text-white" />
           </div>
           <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-2">
-            Yahoo Mail
+            Yahoo Mail Beta
           </h1>
           <p className="text-gray-600 dark:text-gray-400">
             Sign in to access your emails
