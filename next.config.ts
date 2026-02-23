04ba94c9940a3a135d473fe61ae51726fc7155d0
Kevin Patel <patelkev@yahooinc.com>
2026-02-22 17:19:14 -0800

    Added Dockerfile and removed the mastra memory

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  serverExternalPackages: [
+    '@libsql/client',
+    '@mastra/libsql',
+    'pino',
+    'thread-stream',
+    'pino-abstract-transport',
+    'pino-std-serializers',
+    '@mastra/loggers'
+  ],
 };
 
 export default nextConfig;
